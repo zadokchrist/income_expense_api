adee6f1c2964afe24516397d2219eb6580f8be15
zadokchrist <ngobizadokchrist@gmail.com>
2023-01-26 13:21:47 +0300

    setting secret key

--- incomeexpenseapi/settings.py
+++ incomeexpenseapi/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+831d84t96hhov5$_cd%+qjkh*dcks(7&*fwf14e5zm8p=_@sj'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = 'authentication.User'
 # Application definition
